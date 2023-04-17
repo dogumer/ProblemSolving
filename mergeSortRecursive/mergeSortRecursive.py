a7af0c7c3ab45d45997f2223d634253f04a85ae8
Dogukan Meral <dogfishqey@yahoo.com>
2023-04-17 21:11:55 +0300

    Create mergeSortRecursive.py

---
+++ mergeSortRecursive/mergeSortRecursive.py
@@ -0,0 +1,33 @@
+def mergeSortRecursive(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        leftHalf = arr[:mid]
+        rightHalf = arr[mid:]
+
+        mergeSortRecursive(leftHalf)
+        mergeSortRecursive(rightHalf)
+
+        i = j = k = 0
+
+        while i < len(leftHalf) and j < len(rightHalf):
+            if leftHalf[i] < rightHalf[j]:
+                arr[k] = leftHalf[i]
+                i += 1
+            else:
+                arr[k] = rightHalf[i]
+                j += 1
+            k += 1
+
+        while i < len(leftHalf):
+            arr[k] = leftHalf[i]
+            i += 1
+            k += 1
+
+        while j < len(rightHalf):
+            arr[k] = rightHalf[j]
+            j += 1
+            k += 1
+
+        print(arr)
+
+mergeSortRecursive([1,5,8,90,54])
